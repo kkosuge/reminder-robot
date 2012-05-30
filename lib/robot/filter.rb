811b0e8b6dfa337e53ad0ad580ffeac3addd71eb
kkosuge <ksgkzy@gmail.com>
2012-05-30 17:08:56 +0900

    無限リプライ防止フィルター追加

--- lib/robot/filter.rb
+++ lib/robot/filter.rb
@@ -8,13 +8,25 @@ module ReminderRobot
         @include = config.filter.include
         @exclude = config.filter.exclude
         @self_screen_name = config.screen_name
+        @re ||= {}
       end
 
       def catch(status)
         return false unless status.text
         return false unless status.text.match(@include)
         return false if status.text.match(@exclude)
+        return false if runaway(status)
         return true
+      end
+
+      private
+
+      def runaway(status)
+        result = 0
+        @re[Time.now] = status.user.screen_name
+        @re.each { |key,value| @re.delete(key) if key < 1.minutes.ago }
+        @re.each { |key,value| result += 1 if value == status.user.screen_name }
+        result > 4
       end
     end
   end
--- spec/robot/filter.rb
+++ spec/robot/filter.rb
@@ -5,12 +5,16 @@ require File.expand_path(File.dirname(__FILE__) + '../../spec_helper')
 describe ReminderRobot::Robot::Filter do
   before do
     @config = ReminderRobot::ConfigLoader.load
+    @filter = ReminderRobot::Robot::Filter.new(@config)
   end
 
   context 'リプライが含まれなかったとき' do
     before do
-      @status = Hashie::Mash.new(text: "matz")
-      @filter = ReminderRobot::Robot::Filter.new(@config)
+      status = {
+        :text => "matz",
+        :user => { :screen_name => "9m" }
+      }
+      @status = Hashie::Mash.new(status)
     end
     
     it 'false を返す' do
@@ -21,8 +25,11 @@ describe ReminderRobot::Robot::Filter do
   context 'リプライが含まれるとき' do
     context '除外キーワードが含まれるとき' do
       before do
-        @status = Hashie::Mash.new(text: "【拡散】RT @reminder_robot ニャーン")
-        @filter = ReminderRobot::Robot::Filter.new(@config)
+        status = {
+          :text => "【拡散】 RT @reminder_robot ニャーン",
+          :user => { :screen_name => "matz" }
+        }
+        @status = Hashie::Mash.new(status)
       end
 
       it 'false を返す' do
@@ -32,12 +39,69 @@ describe ReminderRobot::Robot::Filter do
 
     context '除外キーワードが含まれない' do
       before do
-        @status = Hashie::Mash.new(text: "@reminder_robot ニャーン")
-        @filter = ReminderRobot::Robot::Filter.new(@config)
+        status = {
+          :text => "@reminder_robot ニャーン",
+          :user => { :screen_name => "T7" }
+        }
+        @status = Hashie::Mash.new(status)
       end
 
       it 'true を返す' do
         @filter.catch(@status).should be_true
+      end
+
+      context '短時間に５回以上リプライが来る' do
+        before do
+          status = {
+            :text => "@reminder_robot ニャーン",
+            :user => { :screen_name => "JustinBieber" }
+          }
+          @status = Hashie::Mash.new(status)
+          5.times { @filter.catch(@status) }
+        end
+
+        it 'false を返す' do
+          @filter.catch(@status).should be_false
+        end
+
+        context '１分間隔を空けた場合' do
+          before do
+            status = {
+              :text => "@reminder_robot ニャーン",
+              :user => { :screen_name => "JustinBieber" }
+            }
+            @status = Hashie::Mash.new(status)
+            5.times { @filter.catch(@status) }
+          end
+
+          it 'true を返す' do
+            pending("１分待つの…")
+            sleep 60
+            @filter.catch(@status).should be_true
+          end
+        end
+
+        context '複数アカウントから来た場合' do
+          before do
+            status = {
+              :text => "@reminder_robot ニャーン",
+              :user => { :screen_name => "masason" }
+            }
+            5.times {
+              @status = Hashie::Mash.new(status)
+              @filter.catch(@status)
+            }
+            status = {
+              :text => "@reminder_robot ニャーン",
+              :user => { :screen_name => "neko" }
+            }
+            @status = Hashie::Mash.new(status)
+          end
+          
+          it 'true を返す' do
+            @filter.catch(@status).should be_true
+          end
+        end
       end
     end
   end
